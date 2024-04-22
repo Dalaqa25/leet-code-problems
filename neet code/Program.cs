65795d958d42574ccd1f82483355af00462e9bb1
Dalaqa25 <160404421+Dalaqa25@users.noreply.github.com>
2024-04-22 20:32:51 +0400

    Add project files.

---
+++ neet code/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neet_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 5 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ()
+            }
+        }
+    }
+}
